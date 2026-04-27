5b300bcd1f1336f13d35e74f5ac5272ce520ea03
JheniferTavares <jhenifer.tavares@sptech.school>
2026-04-27 20:45:25 -0300

    Correção do BD

--- projeto/banco_de_dados/BD_MY-Library.sql
+++ projeto/banco_de_dados/BD_MY-Library.sql
@@ -3,28 +3,38 @@ USE MY_Library;
 
 CREATE TABLE usuario(
     id_usuario INT PRIMARY KEY AUTO_INCREMENT,
-    nome VARCHAR(100),
-    email VARCHAR(100) UNIQUE,
-    senha VARCHAR(100)
+    nome VARCHAR(50) NOT NULL,
+    email VARCHAR(70) UNIQUE NOT NULL,
+    senha VARCHAR(30) NOT NULL
 );
 
 CREATE TABLE genero (
     id_genero INT PRIMARY KEY AUTO_INCREMENT,
     nome VARCHAR(50)
-);
+) AUTO_INCREMENT = 100;
+
+INSERT INTO genero (nome) VALUES
+	('Romance'),
+	('Fantasia'),
+	('Ficção Científica'),
+	('Suspense'),
+	('Aventura'),
+	('Crônica'),
+	('Poesia'),
+	('Infantil'),
+	('Terror');
 
 CREATE TABLE livro (
     id_livro INT PRIMARY KEY AUTO_INCREMENT,
-    titulo VARCHAR(100),
-    autor VARCHAR(100),
+    titulo VARCHAR(50) NOT NULL,
+    autor VARCHAR(50),
     status VARCHAR(20),
-    fk_genero INT,
-    fk_usuario INT,
-    
     CONSTRAINT chk_status 
         CHECK (status IN ('quero_ler', 'lendo', 'lido')),
+    fk_genero INT,
+    fk_usuario INT,
     FOREIGN KEY (fk_genero)
 		REFERENCES genero(id_genero),
     FOREIGN KEY (fk_usuario)
 		REFERENCES usuario(id_usuario)
-);
+) AUTO_INCREMENT = 1000;
